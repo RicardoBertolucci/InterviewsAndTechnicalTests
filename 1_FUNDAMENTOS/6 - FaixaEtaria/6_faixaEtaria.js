d7ce6c8d15c669253943cecca71dc497b20d6ea1
RicardoBertolucci <ricardo.zampolo@traffega.com.br>
2024-01-30 10:06:29 -0300

    NEW: fix exercise 6

--- 1_FUNDAMENTOS/6 - FaixaEtaria/6_faixaEtaria.js
+++ 1_FUNDAMENTOS/6 - FaixaEtaria/6_faixaEtaria.js
@@ -6,8 +6,8 @@ const classificacaoEtaria = (num) => {
         throw new Error(`O valor digitado precisa ser um número!`);
     }
 
-    if(num < 0) return 'Valor inválido!';
-    if(num >= 0 && num < 13) return 'Criança';
+    if(num <= 0) return 'Valor inválido!';
+    if(num > 0 && num < 13) return 'Criança';
     if(num >= 13 && num < 18) return 'Adolescente';
     if(num >= 18 && num < 60) return 'Adulto';
     if(num >= 60) return 'Idoso';
