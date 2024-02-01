ac9dfb08b7876fa32f2ad17bccc9707c9fa9df9b
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-01 18:26:07 -0300

    UPDATE: exercise 16

--- 2_FUNCOES/16 - Fatorial/16_Fatorial.js
+++ 2_FUNCOES/16 - Fatorial/16_Fatorial.js
@@ -1,17 +1,15 @@
 const num = Number(prompt("Digite um número: "));
 
 const fatorial = num => {
-    if(num === 1) return 1;
-    
+    if (num === 1) return 1;
+
     let fat1 = fatorial(num - 1);
 
     return fat1 * num;
-    // return fatorial(num - 1) + fatorial(num - 2);
-    
 }
 
 try {
-    if(isNaN(num) || num <= 0) {
+    if (isNaN(num) || num <= 0) {
         throw new Error("O número digitado é inválido!");
     }
 
