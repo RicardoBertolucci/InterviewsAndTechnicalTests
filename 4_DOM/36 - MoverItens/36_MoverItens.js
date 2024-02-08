b1007fb0b0d73349289f57b2fc678cbc3d88f1b4
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-07 23:15:58 -0300

    NEW: exercise 36

---
+++ 4_DOM/36 - MoverItens/36_MoverItens.js
@@ -0,0 +1,39 @@
+const lista = document.querySelector('.lista');
+
+const li = lista.getElementsByTagName('li');
+
+const buttonTop = document.querySelector('.btn-up');
+const buttonBottom = document.querySelector('.btn-down');
+
+const buttonUp = () => {
+    console.log('Cliquei em subir!');
+}
+
+const buttonDown = () => {
+    console.log('Cliquei em descer!');
+}
+
+
+
+lista.addEventListener('click', (e) => {
+    const el = e.target;
+
+    for (let i = 0 ; i < li.length; i++) {
+        if(li[i].classList.contains('selecionado')) {
+            li[i].classList.remove('selecionado');
+        };
+    };
+
+    el.classList.add('selecionado');
+
+    let escolhido;
+
+    for (let i = 0 ; i < li.length; i++) {
+        if(li[i].classList.contains('selecionado')) {
+            escolhido = li[i];
+        };
+    };
+
+    // buttonTop.addEventListener('click', buttonUp());
+    // buttonBottom.addEventListener('click', buttonDown());
+})
