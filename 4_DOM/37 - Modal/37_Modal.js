7cca18e386ee9451d859be7c5693a0c59d409f7b
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-08 17:19:10 -0300

    UPDATE: commets the code

---
+++ 4_DOM/37 - Modal/37_Modal.js
@@ -0,0 +1,12 @@
+// const buttonOpen = document.querySelector('.open-modal');
+// const buttonClose = document.querySelector('.close-modal');
+// const body = document.querySelector('body');
+// const modal = document.querySelector('.style-modal');
+
+// buttonOpen.addEventListener('click', () => {
+//     modal.style.display = 'flex';
+// });
+
+// buttonClose.addEventListener('click', () => {
+//     modal.style.display = 'none';
+// })
