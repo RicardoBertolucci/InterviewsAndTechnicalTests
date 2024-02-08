3bcba1980a70a6d725ff64668ed0e656e1cbf4ad
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-08 13:39:34 -0300

    FINISH: finished exercise

--- 4_DOM/36 - MoverItens/36_MoverItens.js
+++ 4_DOM/36 - MoverItens/36_MoverItens.js
@@ -1,51 +1,36 @@
 //Minha resolução
-const lista = document.querySelector('.lista');
+/* Incompleta
+const lista = document.querySelector('#listaMover');
+const li = Array.from(lista.querySelectorAll('li'));
+const botaoCima = document.querySelector('#moverCima');
+const botaoBaixo5 = document.querySelector('#moverBaixo');
 
-const li = lista.getElementsByTagName('li');
 
-const buttonTop = document.querySelector('.btn-up');
-const buttonBottom = document.querySelector('.btn-down');
+const selecionaEl = (el) => {
+    li.forEach((liInList) => {
+        if(liInList.classList.contains('selecionado')) {
+            liInList.classList.remove('selecionado');
+        }
+    });
 
-const buttonUp = (buttonTop) => {
-    buttonTop.addEventListener('click', (e) => {
-        console.log('Cliquei em subir!');
-    })
-    
+    el.classList.add('selecionado');
 }
-
-const buttonDown = (buttonBottom) => {
-    console.log('Cliquei em descer!');
-}
-
-
 
 lista.addEventListener('click', (e) => {
     const el = e.target;
-
-    for (let i = 0 ; i < li.length; i++) {
-        if(li[i].classList.contains('selecionado')) {
-            li[i].classList.remove('selecionado');
-        };
-    };
-
-    el.classList.add('selecionado');
-
-    let escolhido;
-
-    for (let i = 0 ; i < li.length; i++) {
-        if(li[i].classList.contains('selecionado')) {
-            escolhido = li[i];
-        };
-    };
-
-
-    
-    buttonTop.addEventListener('click', buttonUp());
-    buttonBottom.addEventListener('click', buttonDown());
+    selecionaEl(el);
 })
 
+botaoCima.addEventListener('click', (e) => {
+    for (let i = 0; i < li.length; i++) {
+        if(li.indexOf('selecionado') === 1) {
 
-/*Resolução Dele 
+        }
+    }
+})
+*/
+
+//Resolução Dele 
 function moverItem(direcao) {
     const lista = document.querySelector("#listaMover");
     const itens = Array.from(lista.querySelectorAll("li"));
@@ -82,4 +67,3 @@ const botaoBaixo = document.querySelector('#moverBaixo');
 
 botaoCima.addEventListener("click", () => moverItem("cima"));
 botaoBaixo.addEventListener("click", () => moverItem("baixo"));
-*/
