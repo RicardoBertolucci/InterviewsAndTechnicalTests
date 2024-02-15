f5e6f0a4392e1b46e4b497043a405daa1c80413a
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-14 22:51:38 -0300

    NEW: continue exercise

---
+++ 5_ASYNCFUNCTIONS/45 - RetryPromises/45_RetryPromises.js
@@ -0,0 +1,14 @@
+const acessoAPI = () => {
+
+}
+
+acessoAPI (
+    API = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+    },
+    () => {
+        let max = 3;
+        let time = 5;
+    }
+
+)
