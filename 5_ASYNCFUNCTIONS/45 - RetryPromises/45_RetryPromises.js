57ebc1c658217321d0025b6983b143ce6f918d8e
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-15 11:04:25 -0300

    AFTER: continue

--- 5_ASYNCFUNCTIONS/45 - RetryPromises/45_RetryPromises.js
+++ 5_ASYNCFUNCTIONS/45 - RetryPromises/45_RetryPromises.js
@@ -1,14 +1,17 @@
-const acessoAPI = () => {
+// const acessoAPI = async (api, max, temp) => {
 
-}
+//     for (let i = 0; i < max; i++) {
+        
+//         setTimeout(executaChamada(temp), 1000)
+//         // const resposta = await executaChamada(api, temp);
 
-acessoAPI (
-    API = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts/1')
-    },
-    () => {
-        let max = 3;
-        let time = 5;
-    }
+//     }
+// }
 
-)
+// acessoAPI(
+//     async () => {
+//         return fetch('https://jsonplaceholder.typicode.com/posts/1').then((dados) => console.log(dados));
+//     },
+//     3,
+//     5
+// );
